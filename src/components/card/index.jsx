5c7f1a3f5939d6461e78accc72daaa0370065007
Abraones <abraaobenites@gmail.com>
2020-11-25 17:03:42 -0300

    criacao das pastas pages, components e esboco do card

---
+++ src/components/card/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './card.css'
+
+export function Card(props){
+    const titulo = props.title
+    const texto = props.text
+    const qntde = props.qnt
+    return (
+        <button>
+            <h1 className="title" >{titulo}</h1>
+             <p className="text">{texto}</p>
+            <p className="qnt" >{qntde}:</p> 
+        </button>
+    )
+}
+
