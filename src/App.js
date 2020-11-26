02e7b282d46a69f84fbd5726faa634bb479efd02
danilo Lima Dias <danilo743377@gmail.com>
2020-11-26 15:49:33 -0300

    componente despensa vazia finalizada

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
-import {EmptyList} from './components/EmptyList/EmptyList'
-import  ImgEmptyList  from './assets/img/ImgEmptyList.svg'
 import './App.css';
 
 
 function App() {
-  return (
-    <div className="App">
-     <EmptyList 
-     src={ImgEmptyList}
-     alt="mulher fazendo"
-     desc="Ops! A sua despensa está vazia."
-     subTitle="Que tal adicionar itens agora?"
-     />
-    </div>
-  );
+  return 
 }
 
 export default App;
--- src/components/EmptyList/EmptyList.jsx
+++ src/components/EmptyList/EmptyList.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import {Button} from '../Button/Button';
+import  ImgEmptyList  from '../../assets/img/ImgEmptyList.svg'
+
 import './EmptyList.scss'
 
 export function EmptyList(props) {
-  const {src, alt, desc, subTitle} = props
+  const { alt, desc, subTitle} = props
 
   return (
     <div className="container-emptyList">
-      <img src={src} alt={alt}/>
+      <img src={ImgEmptyList} alt={alt}/>
       <p>{desc}</p>
       <h2>{subTitle}</h2>
       <Button value="Vamos lá!"/>
