16220ebfc1f2c26fbd5779249a6829a30f8cd8f4
Abraones <abraaobenites@gmail.com>
2020-11-26 22:38:02 -0300

    card estilizado, faltando apenas a flexinha

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
+
 import './card.scss'
 
 export function Card(props){
+    //const {titulo, children, qntde, source} = props
+
     const titulo = props.title
-    const texto = props.text
+    const texto = props.children
     const qntde = props.qnt
+    const source = props.src
+    
+    
+
     return (
-        <button>
+    <> 
+        {/*<img src={carrinho} />*/}
+
+        <button style={{backgroundColor: props.color}}>
             <h1 className="title" >{titulo}</h1>
              <p className="text">{texto}</p>
             <p className="qnt" >{qntde}:</p> 
+            <img className="abso" src={source}/>
         </button>
+    </>
     )
 }
 
