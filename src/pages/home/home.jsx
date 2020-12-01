46dc31f2af620ac0b9ec0206ebdf1457d861e53f
Abraones <abraaobenites@gmail.com>
2020-11-30 22:14:10 -0300

    tentativa de aparecer a imagem

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -9,11 +9,11 @@ export function HomePage(){
     return (
         <>
           <Link to="/despensavazia">
-            <Card  title="Despensa" qnt="Quantidade de itens:" src={despensa} color="rgba(131, 163, 144, 0.85)">
+            <Card  title="Despensa" qnt="Quantidade de itens:" src={despensa} color="rgba(131, 163, 144)">
                   Confira todos os itens disponíveis na sua despensa ou adicione novos!
             </Card>
           </Link>
-          <Card  title="Lista de Compra" qnt="Quantidade de itens:" src={carrinho} color="rgba(209, 203, 189, 0.85" >
+          <Card  title="Lista de Compra" qnt="Quantidade de itens:" src={carrinho} color="rgba(209, 203, 189" >
                   Lembre-se de atualizar sua lista antes de fazer as compras!
           </Card>
           <Navbar />
