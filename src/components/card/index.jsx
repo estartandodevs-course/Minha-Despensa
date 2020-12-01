c738ec6a4c57be0492ab6ed508eb69ab32336892
Abraones <abraaobenites@gmail.com>
2020-11-30 21:14:30 -0300

    arrow no card e ajuste de espacamento

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import arrow from '../../assets/icons/arrow.svg'
 
 import './card.scss'
 
@@ -14,12 +15,13 @@ export function Card(props){
 
     return (
     <> 
-        {/*<img src={carrinho} />*/}
+        {/*<img src={carrinho} />*/} 
 
         <button style={{backgroundColor: props.color}}>
             <h1 className="title" >{titulo}</h1>
              <p className="text">{texto}</p>
             <p className="qnt" >{qntde}:</p> 
+            <img className="arrow" src={arrow} alt="Flecha"/>
             <img className="abso" src={source} alt="Legumes"/>
         </button>
     </>
