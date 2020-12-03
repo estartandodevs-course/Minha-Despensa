3da5d32da8e78bbec7a5c114ae5b62947f755181
danilo Lima Dias <danilo743377@gmail.com>
2020-12-03 15:03:51 -0300

    componente checkbox finalizado

--- src/pages/Form/form.jsx
+++ src/pages/Form/form.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Header } from '../../components/header/header'
 import { Navbar } from '../../components/navbar/navbar'
-import {Checkbox} from '../../components/checkbox/checkbox'
+import { Checkbox } from '../../components/checkbox/checkbox'
 
 export function FormItens() {
   return (
     <>
-    <Header text="Inserir item na despensa"/> 
-    <Checkbox/>
-    
+      <Header text="Inserir item na despensa" />
       <Navbar />
     </>
   )
