7ebaff32be610570264d84e1833c07e4c96d0151
danilo Lima Dias <danilo743377@gmail.com>
2020-12-03 15:20:33 -0300

    concertando erro do deploy

--- src/pages/Form/form.jsx
+++ src/pages/Form/form.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { Header } from '../../components/header/header'
 import { Navbar } from '../../components/navbar/navbar'
 
