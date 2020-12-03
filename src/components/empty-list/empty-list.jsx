bd79c699e97775f5ce53a4df589b668f23ff1acb
Abraão Lincon <70493681+Abraones@users.noreply.github.com>
2020-12-02 21:43:04 -0300

    mudança de "b" para "B"

--- src/components/empty-list/empty-list.jsx
+++ src/components/empty-list/empty-list.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Button }  from "../button/Button"
+import { Button }  from "../Button/Button"
 
 
 import ImgEmptyList from '../../assets/icons/ImgEmptyList.svg'
