9e10eeb0771ee40972a30840847bd8e6ab40efd0
Abraão Lincon <70493681+Abraones@users.noreply.github.com>
2020-12-05 17:42:25 -0300

    anotando imports

--- src/pages/Form/form.jsx
+++ src/pages/Form/form.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Header } from '../../components/header/header'
 import { Navbar } from '../../components/navbar/navbar'
-import { DropDownAb } from '../../components/inputs-form/dropdown/drop-down';
-  const medidas = [ 'pct', 'kg', 'g', 'cx' ]
-  const categorias = ['Limpeza', 'Mercearia', 'Perfumaria']
+//import { DropDownAb } from '../../components/inputs-form/dropdown/drop-down';
+  //const medidas = [ 'pct', 'kg', 'g', 'cx' ]
+  //const categorias = ['Limpeza', 'Mercearia', 'Perfumaria']
 export function FormItens() {
 
   return (
