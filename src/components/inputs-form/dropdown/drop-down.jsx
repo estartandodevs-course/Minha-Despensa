6e3bfb53c84a5b6a0db7992a0885a5cb596f6af1
Abraones <abraaobenites@gmail.com>
2020-12-04 23:12:18 -0300

    componente DropDown criado e colocado no form

--- src/components/inputs-form/dropdown/drop-down.jsx
+++ src/components/inputs-form/dropdown/drop-down.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
+import DropDown from 'react-dropdown'
+import './drop-down.scss'
+import arrow from '../../../assets/icons/arrow-down.svg'
 
-import 'react-dropdown/style.css';
- 
 
+export function DropDownAb(props){
+    const {width, options, title, placeholder} = props
 
-export function DropDown(props){
-    
     return(
         <>
-        
-        
+            <div className="container-select">
+                <h2>{title}</h2>
+                <img className='arrow' src={arrow} alt="arrow to down"/>
+                <DropDown 
+                    className='select' 
+                    placeholderClassName='place' 
+                    menuClassName='menu'
+                    controlClassName='control'
+                    options={options} /* onChange={this._onSelect} */ 
+                    /* value={}  */
+                    placeholder={placeholder}
+                />
+                
+            </div>
         </>
     )
 }
