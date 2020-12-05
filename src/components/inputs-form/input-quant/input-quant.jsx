21d1111c4dc1544ab98933c2399305720f88baaf
LuizEduardoBP <lepessoa38@gmail.com>
2020-12-05 14:14:13 -0300

    Estilização do input e troca de cor

--- src/components/inputs-form/input-quant/input-quant.jsx
+++ src/components/inputs-form/input-quant/input-quant.jsx
@@ -1,10 +1,17 @@
 import { Icon } from "../../navbar/icon-navbar/icon-navbar"
 import "./input-quant.scss"
 import iconMais from "../../../assets/icons/icon-mais.svg"
+import iconMaisVerde from "../../../assets/icons/icon-mais-verde.svg"
 import iconMenos from "../../../assets/icons/icon-menos.svg"
+import iconMenosVerde from "../../../assets/icons/icon-menos-verde.svg"
 import { useState } from "react"
+
 export function Quant(){
     const [quant, setQuant] = useState(0)
+    const [mais, setMais] = useState(iconMais)
+    const [menos, setMenos] = useState(iconMenos)
+    const [num, setNum] = useState({color: "#c48b76"})
+    const [title, setTitle] = useState({color: "#c48b76"})
     
     function Mais(){
         setQuant(quant+1)
@@ -22,13 +29,30 @@ export function Quant(){
         setQuant(parseInt(qnt.target.value))
      }
 
+        function onFocus(){
+            setMais(iconMaisVerde)
+            setMenos(iconMenosVerde)
+            setNum({color: "#437056"})
+            setTitle({color: "#437056"})
+        }
+
+        function onFocusOut(){
+            setMais(iconMais)
+            setMenos(iconMenos)
+            setNum({color: "#c48b76"})
+            setTitle({color: "#c48b76"})
+        }
+
+        
+
+
     return(
         <>
-        <h2 className="quant-title"> Qtde. </h2>
+        <h2 style={title} className="quant-title"> Qtde. </h2>
         <div className="quant-section">
-            <Icon onClick={Menos} className={"icon-menos"} src={iconMenos}/>
-            <input onChange={onChange} className="input-quant" type="number" value={quant}/>
-            <Icon onClick={Mais} className={"icon-mais"} src={iconMais}/>
+            <Icon onClick={Menos} className={"icon-menos"} src={menos}/>
+            <input style={num} onFocus={onFocus} onBlur={onFocusOut} onChange={onChange} className="input-quant" type="number" value={quant}/>
+            <Icon onClick={Mais} className={"icon-mais"} src={mais}/>
         </div>
         </>
     )
--- src/pages/Form/form.jsx
+++ src/pages/Form/form.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Header } from '../../components/header/header'
+import { Quant } from '../../components/inputs-form/input-quant/input-quant'
 import { Navbar } from '../../components/navbar/navbar'
 
 
@@ -7,6 +8,7 @@ export function FormItens() {
   return (
     <>
       <Header text="Inserir item na despensa" />
+      <Quant />
       <Navbar />
     </>
   )
