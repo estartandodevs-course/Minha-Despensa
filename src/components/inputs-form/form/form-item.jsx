b3ec5a0be3c485d0ebd5a68633a3c6e28fbd1d03
Abraones <abraaobenites@gmail.com>
2020-12-09 18:29:36 -0300

    Nome, Qte, unt, date, category in the object

--- src/components/inputs-form/form/form-item.jsx
+++ src/components/inputs-form/form/form-item.jsx
@@ -6,7 +6,7 @@ import { Calendar } from "../calendar/calendar";
 //import { Checkbox } from "../checkbox/checkbox";
 import { ProductName } from "../product-name/product-name";
 import { DropDownAb } from "../dropdown/drop-down";
-//import { Quant } from "../input-quant/input-quant";
+import { Quant } from "../input-quant/input-quant";
 import { Button } from "../../Button/Button";
 //import { itens } from "../../../_mocks/mocks";
 
@@ -16,16 +16,22 @@ const options = [
   { value: "g" },
   { value: "cx" },
 ];
-//const categorias = ["Limpeza", "Mercearia", "Perfumaria"];
+const categorias = [
+  { value: "Limpeza" },
+  { value: "Mercearia" },
+  { value: "Perfumaria" },
+];
 
 export function FormItens(props) {
   const [form, setForm] = useState({});
   const [unita, setUnita] = useState({});
+  const [catego, setCatego] = useState({});
 
   function handleChange(e) {
     const name = e.target.name;
     const value = e.target.value;
-    const formulario = { ...form, unit: unita };
+    const formulario = { ...form, unit: unita, category: catego, };
+    //const som = parseInt(e.target.value)
 
     setForm({
       ...formulario,
@@ -34,38 +40,40 @@ export function FormItens(props) {
   }
   function handleChangeDropbox(e) {
     const value = e.value;
-
     setUnita(value);
   }
+  function handleChangeDropbox2(e) {
+    const value = e.value;
+    setCatego(value);
+  }
+
 
   return (
     <div>
       <ProductName name="name" label="nome" onChange={handleChange} />
 
-      {/* <div style={{ display: "flex" }}> */}
-      <DropDownAb
-        name="category"
-        onChange={handleChangeDropbox}
-        className="w328"
-        arrowWidth="arrow328"
-        title="Categoria:"
-        placeholder="Escolha uma categoria"
-        options={options}
-      />
-      {/* <Quant /> */}
-
-      <DropDownAb
-        name="category"
-        onChange={handleChangeDropbox}
-        className="w328"
-        arrowWidth="arrow328"
-        title="Categoria:"
-        placeholder="Escolha uma categoria"
-        options={options}
-      />
-      {/* </div> */}
-
+      <div style={{ display: "flex" }}>
+         <Quant onChange={handleChange} name="qnt" />
+        <DropDownAb
+          name="category"
+          onChange={handleChangeDropbox}
+          className="w190"
+          arrowWidth="arrow190"
+          title="Unidades de medida:"
+          placeholder="Escolha uma unidade"
+          options={options}
+        />
+      </div>
       <Calendar name="date" onChange={handleChange} />
+      <DropDownAb
+        name="category"
+        onChange={handleChangeDropbox2}
+        className="w328"
+        arrowWidth="arrow328"
+        title="Categoria:"
+        placeholder="Escolha uma categoria"
+        options={categorias}
+      />
 
       {/* <Checkbox name="stateItem" onChange={handleChange} /> */}
 
