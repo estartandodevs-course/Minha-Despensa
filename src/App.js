f7fd017d41638ffca39aaa54258e5fe821c8f55c
danilo Lima Dias <danilo743377@gmail.com>
2020-12-09 18:23:33 -0300

    corrigindo erro do deploy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
-import { HomePage } from './pages/home/home'
-import { MinhaDespensa } from './pages/despensa/despensa'
-import { FormItens } from './pages/Form/form';
-import { DespensaVazia } from './pages/despensa-vazia/despensa-vazia';
-import { LoginPage } from './pages/login/login';
+import {
+  HomePage
+} from './pages/home/home'
+import {
+  MinhaDespensa
+} from './pages/despensa/despensa'
+import {
+  FormItens
+} from './pages/Form/form';
+import {
+  DespensaVazia
+} from './pages/despensa-vazia/despensa-vazia';
+import {
+  LoginPage
+} from './pages/login/login';
 
 import {
   BrowserRouter as Router,
@@ -11,31 +21,41 @@ import {
   Route,
 } from "react-router-dom";
 
-export function App(){
-  return (
-    <>
-    <Router>
-      <Switch>
-        <Route path="/despensavazia">
-          <DespensaVazia />
-        </Route>
-      <Route path="/form">
-          <FormItens />
-        </Route>
-        <Route path="/despensa">
-          <MinhaDespensa />
-        </Route>
-        <Route path="/login">
-          <LoginPage />
-        </Route>
-        <Route path="/">
-          <HomePage />
-        </Route>
-
-      </Switch>
-    </Router>
-    </>
+export function App() {
+  return ( <
+    >
+    <
+    Router >
+    <
+    Switch >
+    <
+    Route path = "/despensavazia" >
+    <
+    DespensaVazia / >
+    <
+    /Route> <
+    Route path = "/form" >
+    <
+    FormItens / >
+    <
+    /Route> <
+    Route path = "/despensa" >
+    <
+    MinhaDespensa / >
+    <
+    /Route> <
+    Route path = "/login" >
+    <
+    LoginPage / >
+    <
+    /Route> <
+    Route path = "/" >
+    <
+    HomePage / >
+    <
+    /Route> <
+    /Switch> <
+    /Router> <
+    />
   );
 }
-
- 
--- src/components/inputs-form/product-name/product-name.jsx
+++ src/components/inputs-form/product-name/product-name.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./product-name.scss";
 
 export function ProductName(props) {
