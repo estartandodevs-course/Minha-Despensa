bea5cc84cb3d4154ec6afdc171f0c3c37e6f9d2f
LuizEduardoBP <lepessoa38@gmail.com>
2020-12-14 14:43:26 -0300

    Deletar itens da despensa funcionando

--- src/pages/Form/form.jsx
+++ src/pages/Form/form.jsx
@@ -1,21 +1,26 @@
 import React, { useState } from "react";
 import "./form.scss";
 import { FormItens } from "../../components/inputs-form/form/form-item";
-// import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
-
-// export function FormPage() {
-//   const location = useLocation();
-//   const item = location.state;
-//   if (item !== undefined) {
-//     console.log(item);
-//   }
-
 import { ModalDelete } from "../../components/modal/modal";
 import { Navbar } from "../../components/navbar/navbar";
 import { Header } from "../../components/header/header";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 
 export function FormPage() {
   const [modalIsOpen, setModalOpen] = useState(false);
+  const location = useLocation();
+
+  function Delete() {
+    if (location.state !== null) {
+      const index = location.state.key;
+      const jsonItem = JSON.parse(localStorage.getItem("Item"));
+      jsonItem.splice(index, 1);
+      localStorage.setItem("Item", JSON.stringify(jsonItem));
+    }
+
+    setModalOpen(false);
+  }
+
   return (
     <>
       <Header openDeleteModal={() => setModalOpen(true)} />
@@ -23,6 +28,7 @@ export function FormPage() {
         <ModalDelete
           isOpen={modalIsOpen}
           onCancel={() => setModalOpen(false)}
+          onDelete={() => Delete()}
         />
         <FormItens />
       </main>
