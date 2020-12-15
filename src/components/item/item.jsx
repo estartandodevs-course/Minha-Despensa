55c781cc9a40bc62f7600aee26f1e13a365f419d
Abraones <abraaobenites@gmail.com>
2020-12-15 00:13:27 -0300

    Cor funcionando, falta colocar o texto em um State

--- src/components/item/item.jsx
+++ src/components/item/item.jsx
@@ -1,16 +1,21 @@
 import { Icon } from "../navbar/icon-navbar/icon-navbar";
 import calendarIcon from "../../assets/icons/calendar-icon.svg";
+import calendarRed from "../../assets/icons/calendar-red.svg";
+import calendarOrange from "../../assets/icons/calendar-orange.svg";
+
 import greenFlag from "../../assets/icons/green-flag.svg";
 import redFlag from "../../assets/icons/red-flag.svg";
 import greenMarket from "../../assets/icons/green-market.svg";
 import redMarket from "../../assets/icons/red-market.svg";
 import "./item.scss";
 import { useState, useEffect } from "react";
+import moment from "moment";
 
 export function Item(props) {
   const { src, alt, name, qnt, stateItem, date, onClick } = props;
   const [market, setMarket] = useState(redMarket);
   const [flag, setFlag] = useState(greenFlag);
+  const [calendar, setCalendar] = useState(calendarIcon);
 
   function flagColor() {
     if (qnt === 0) {
@@ -21,6 +26,7 @@ export function Item(props) {
   }
 
   useEffect(flagColor);
+  useEffect(catchDate);
 
   function qntItem() {
     if (qnt === 0) {
@@ -29,6 +35,69 @@ export function Item(props) {
       return { backgroundColor: "#ED6807" };
     } else {
       return { backgroundColor: "#437056" };
+    }
+  }
+
+  let validade = "a";
+  function catchDate() {
+    //let validDate = date
+    let currentDateJS = new Date();
+    const dayJS = currentDateJS.getDate();
+    const monthJS = currentDateJS.getMonth() + 1;
+    const yearJS = currentDateJS.getFullYear();
+    const actualDate = `${monthJS}-${dayJS}-${yearJS}`;
+
+    //const DataAtual = moment(actualDate);
+
+    const dateArray = date.match(/\d/g);
+    const itemDay = dateArray[6] + dateArray[7];
+    const itemMonth = dateArray[4] + dateArray[5];
+    const itemYear = dateArray[0] + dateArray[1] + dateArray[2] + dateArray[3];
+    const itemDate = ` ${itemMonth}-${itemDay}-${itemYear}`;
+
+    const isFresh = moment(itemDate).isAfter(actualDate);
+    //const itemFresh = moment(itemDate).diff(actualDate, "d")
+    //const itemUnfresh = moment(actualDAte).diff(itemlDate, "d")
+    console.log(moment(itemDate).isAfter(actualDate));
+
+    if (isFresh) {
+      let itemFresh = moment(itemDate).diff(actualDate, "d");
+      if (itemFresh > 7) {
+        itemFresh = moment(itemDate).diff(actualDate, "week");
+        validade = `Em ${itemFresh} semanas`;
+        //console.log(validade);
+        setCalendar(calendarIcon);
+        return `Em ${itemFresh} semanas`;
+      }
+      if (itemFresh === 1) {
+        validade = `Em ${itemFresh} dia`;
+        //console.log(validade);
+        return validade;
+      }
+      validade = `Em ${itemFresh} dias`;
+      //console.log(validade);
+      setCalendar(calendarOrange);
+      return validade;
+    } else {
+      let itemUnfresh = moment(actualDate).diff(itemDate, "d");
+      setCalendar(calendarRed);
+      if (itemUnfresh > 7) {
+        itemUnfresh = moment(actualDate).diff(itemDate, "w");
+        validade = `Há ${itemUnfresh} semanas`;
+        //console.log(validade);
+        return validade;
+      }
+
+      if (itemUnfresh === 1) {
+        itemUnfresh = moment(actualDate).diff(itemDate, "d");
+        validade = `Há ${itemUnfresh} dia`;
+        //console.log(validade);
+        return validade;
+      }
+
+      validade = `Há ${itemUnfresh} dias`;
+      //console.log(validade);
+      return validade;
     }
   }
 
@@ -41,6 +110,7 @@ export function Item(props) {
   //         setMarket(redMarket)
   //     }
   // }
+  //catchDate();
 
   return (
     <div onClick={onClick} className="card-item-container">
@@ -57,8 +127,8 @@ export function Item(props) {
           <p className="text-item">{stateItem}</p>
         </div>
         <div className="flag-text">
-          <Icon className="icon" src={calendarIcon} />
-          <p className="text-item">{date}</p>
+          <Icon className="icon" src={calendar} />
+          <p className="text-item">{validade}</p>
           <Icon
             src={market}
             onClick={() =>
@@ -67,6 +137,7 @@ export function Item(props) {
                 : setMarket(redMarket)
             }
           />
+          <button onClick={catchDate}></button>
         </div>
       </div>
     </div>
